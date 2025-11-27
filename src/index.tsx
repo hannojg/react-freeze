e8ef497a1d03e7764ed3b4870bceae4b38daf87e
Hanno J. Gödecke <die.drei99@yahoo.de>
2025-11-27 17:27:21 +0100

    fix: fix endless loop by using stable `use()` API for suspending

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React, { Suspense, Fragment } from "react";
-
-const infiniteThenable = { then() {} };
+import React, { Suspense, Fragment, use } from "react";
 
 interface SuspenderProps {
   freeze: boolean;
@@ -8,9 +6,30 @@ interface SuspenderProps {
 }
 
 function Suspender({ freeze, children }: SuspenderProps) {
-  if (freeze) {
-    throw infiniteThenable;
+  const resolverRef = React.useRef<(() => void) | undefined>(undefined);
+  const promiseRef = React.useRef<Promise<void> | null>(null);
+
+  if (promiseRef.current === null && freeze) {
+    promiseRef.current = new Promise<void>((resolve) => {
+      resolverRef.current = resolve;
+    });
   }
+
+  if (!freeze && resolverRef.current != null) {
+    resolverRef.current();
+  }
+
+  if (promiseRef.current !== null) {
+    use(promiseRef.current);
+  }
+
+  if (!freeze) {
+    // Only reset promise here, as when un-freezing we want to "ping the attached listeners" that the promise is resolved.
+    // Thats why we call the resolver above when !freeze.
+    promiseRef.current = null;
+    resolverRef.current = undefined;
+  }
+
   return <Fragment>{children}</Fragment>;
 }
 
